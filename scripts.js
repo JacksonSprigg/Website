e2b2c604964e177c8013300c70ec0e7b9da8a583
Jackson <jacksonsprigg@gmail.com>
2023-11-13 21:50:24 +1100

    Updates

--- scripts.js
+++ scripts.js
@@ -11,3 +11,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 
+// Function to toggle theme and save preference
+function toggleTheme() {
+    let theme = document.body.getAttribute('data-theme');
+    let newTheme = theme === 'light' ? 'dark' : 'light';
+    document.body.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+// Event listener for theme change
+document.getElementById('theme-toggle-checkbox').addEventListener('change', toggleTheme);
+
+// Function to apply saved theme on page load
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.setAttribute('data-theme', savedTheme);
+    }
+}
+
+// Call applySavedTheme on page load
+document.addEventListener('DOMContentLoaded', applySavedTheme);
